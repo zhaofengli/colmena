87189ec3df17c517cdcc806b8b7b14d6f5f0a372
Zhaofeng Li <hello@zhaofeng.li>
2022-01-22 17:50:53 -0800

    nix_info: Display pinned nix-eval-jobs path

--- src/command/nix_info.rs
+++ src/command/nix_info.rs
@@ -2,6 +2,7 @@ use clap::{App, ArgMatches};
 
 use crate::error::ColmenaError;
 use crate::nix::NixCheck;
+use crate::nix::evaluator::nix_eval_jobs::get_pinned_nix_eval_jobs;
 
 pub fn subcommand() -> App<'static> {
     App::new("nix-info")
@@ -12,6 +13,12 @@ pub async fn run(_global_args: &ArgMatches, _local_args: &ArgMatches) -> Result<
     let check = NixCheck::detect().await;
     check.print_version_info();
     check.print_flakes_info(false);
+
+    if let Some(pinned) = get_pinned_nix_eval_jobs() {
+        log::info!("Using pinned nix-eval-jobs: {}", pinned);
+    } else {
+        log::info!("Using nix-eval-jobs from PATH");
+    }
 
     Ok(())
 }
