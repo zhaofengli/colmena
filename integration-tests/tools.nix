c1dc482c21aeca7a5d0f41ded80b52d904efcebe
Zhaofeng Li <hello@zhaofeng.li>
2022-05-13 19:14:58 -0700

    integration-tests: Add timeouts to SSH invocations
    They occasionally become stuck, similar to NixOS/nixpkgs#171488.

--- integration-tests/tools.nix
+++ integration-tests/tools.nix
@@ -145,7 +145,7 @@ let
 
       for node in ${targetList}:
           node.wait_for_unit("sshd.service")
-          deployer.succeed(f"ssh -o StrictHostKeyChecking=accept-new {node.name} true")
+          deployer.succeed(f"ssh -o StrictHostKeyChecking=accept-new {node.name} true", timeout=30)
 
       deployer.succeed("cp --no-preserve=mode -r ${bundle} /tmp/bundle && chmod u+w /tmp/bundle")
 
