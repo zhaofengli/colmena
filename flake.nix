34c20178c41234a69534b17ff5707ca4a7dd5ffc
Timothy DeHerrera <tim@nrdxp.dev>
2022-05-25 17:55:39 -0600

    flake.nix: add project binary cache to nix.conf

--- flake.nix
+++ flake.nix
@@ -73,4 +73,13 @@
     };
     inherit (evalNix) nixosModules;
   };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://colmena.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
+    ];
+  };
 }
