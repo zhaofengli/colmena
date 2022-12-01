92f0f155d4fac347066240b858a5c77a32149269
Zhaofeng Li <hello@zhaofeng.li>
2022-12-01 01:57:56 -0700

    flake.nix: Bump stable to 22.11, update unstable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    stable.url = "github:NixOS/nixpkgs/nixos-22.05";
+    stable.url = "github:NixOS/nixpkgs/nixos-22.11";
 
     flake-utils.url = "github:numtide/flake-utils";
 
