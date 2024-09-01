aeccdba3b4f4729eea84d98bfad7a2c2a79cb331
Zhaofeng Li <hello@zhaofeng.li>
2024-09-01 13:06:00 -0400

    flake: Update stable to NixOS 24.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    stable.url = "github:NixOS/nixpkgs/nixos-23.05";
+    stable.url = "github:NixOS/nixpkgs/nixos-24.05";
 
     flake-utils.url = "github:numtide/flake-utils";
 
