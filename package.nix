c021f1d26ce3324d7c48c1050c8bbd82e9c6be48
Lord-Valen <lord_valen@protonmail.com>
2025-02-17 15:22:59 -0500

    add meta.mainProgram

--- package.nix
+++ package.nix
@@ -49,5 +49,6 @@ rustPlatform.buildRustPackage rec {
     license = licenses.mit;
     maintainers = with maintainers; [ zhaofengli ];
     platforms = platforms.linux ++ platforms.darwin;
+    mainProgram = "colmena";
   };
 }
