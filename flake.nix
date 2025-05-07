76c974b5d5a6a2a5d85020f35aa52bee648e6b26
Zhaofeng Li <hello@zhaofeng.li>
2025-05-07 10:14:04 -0600

    Update nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    stable.url = "github:NixOS/nixpkgs/nixos-24.05";
+    stable.url = "github:NixOS/nixpkgs/nixos-24.11";
 
     nix-github-actions = {
       url = "github:nix-community/nix-github-actions";
